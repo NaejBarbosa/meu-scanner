10c492cd94c2bed0a15714ddb3e85c3d4e3f5d42
NaejBarbosa <emaildojeann@gmail.com>
2026-06-08 13:02:33 -0300

    Ajuste de tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // 👈 ESSENCIAL para o modo escuro funcionar
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
